617fc41862eeb6092a3ae8716d9a78723e841a47
MiguelC29 <miguelito28c@gmail.com>
2024-02-24 15:07:11 -0500

    adding classes: Category, Product, Provider y User

---
+++ src/main/java/com/felysoft/felysoftApp/entities/Category.java
@@ -0,0 +1,27 @@
+package com.felysoft.felysoftApp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long idCategory;
+
+    @Column(length = 45, nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products;
+}
