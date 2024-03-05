f70e6db7c25fbb61f3188c934a3e323a2c5f31c1
Andre391 <150189948+Andre391@users.noreply.github.com>
2024-03-05 16:31:24 -0500

    Add service Author, Book, Genre and Reserve

---
+++ src/main/java/com/felysoft/felysoftApp/services/BookService.java
@@ -0,0 +1,12 @@
+package com.felysoft.felysoftApp.services;
+
+import com.felysoft.felysoftApp.entities.Book;
+
+import java.util.List;
+
+public interface BookService {
+    public List<Book> findAll() throws Exception;
+    public void create(Book book);
+    public void update(Book book);
+    public void delete(Book book);
+}
---
+++ src/main/java/com/felysoft/felysoftApp/services/GenreService.java
@@ -0,0 +1,12 @@
+package com.felysoft.felysoftApp.services;
+
+import com.felysoft.felysoftApp.entities.Genre;
+
+import java.util.List;
+
+public interface GenreService {
+    public List<Genre> findAll() throws Exception;
+    public void create(Genre genre);
+    public void update(Genre genre);
+    public void delete(Genre genre);
+}
