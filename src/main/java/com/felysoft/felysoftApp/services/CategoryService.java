175a013261765c3a8cd13e3005dd667a22ed3e4d
MiguelC29 <miguelito28c@gmail.com>
2024-03-05 17:21:10 -0500

    Add method findById in service category, inventory, noveltyInv,product, provider

--- src/main/java/com/felysoft/felysoftApp/services/CategoryService.java
+++ src/main/java/com/felysoft/felysoftApp/services/CategoryService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface CategoryService {
     public List<Category> findAll() throws Exception;
+    public Category findById(Long id);
     public void create(Category category);
     public void update(Category category);
     public void delete(Category category);
--- src/main/java/com/felysoft/felysoftApp/services/InventoryService.java
+++ src/main/java/com/felysoft/felysoftApp/services/InventoryService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface InventoryService {
     public List<Inventory> findAll() throws Exception;
+    public Inventory findById(Long id);
     public void create(Inventory inventory);
     public void update(Inventory inventory);
     public void delete(Inventory inventory);
