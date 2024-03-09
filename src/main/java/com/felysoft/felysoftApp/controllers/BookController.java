278f7c1a728d271c1e0a4dbd87a4e95e0b9ba907
Andre391 <150189948+Andre391@users.noreply.github.com>
2024-03-09 17:51:19 -0500

    Controllers Author,book, genre, reserve

---
+++ src/main/java/com/felysoft/felysoftApp/controllers/BookController.java
@@ -0,0 +1,75 @@
+package com.felysoft.felysoftApp.controllers;
+
+import com.felysoft.felysoftApp.entities.Author;
+import com.felysoft.felysoftApp.entities.Book;
+import com.felysoft.felysoftApp.entities.Genre;
+import com.felysoft.felysoftApp.services.imp.AuthorImp;
+import com.felysoft.felysoftApp.services.imp.BookImp;
+import com.felysoft.felysoftApp.services.imp.GenreImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+@RestController
+@RequestMapping(path = "/api/book/", method = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.HEAD})
+@CrossOrigin("*")
+public class BookController {
+    @Autowired
+    private BookImp bookImp;
+    @Autowired
+    private GenreImp genreImp;
+    @Autowired
+    private AuthorImp authorImp;
+    @GetMapping("all")
+    public ResponseEntity<Map<String, Object>> findAll(){
+        Map<String,Object> response= new HashMap<>();
+
+        try{
+            List<Book> bookList= this.bookImp.findAll();
+            response.put("status","success");
+            response.put("data", bookList);
+        }catch (Exception e){
+            response.put("status", HttpStatus.BAD_GATEWAY);
+            response.put("data", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_GATEWAY);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+    @PostMapping("create")
+    public ResponseEntity<Map<String, Object>> create(@RequestBody Map<String,Object> request){
+        Map<String,Object> response= new HashMap<>();
+
+        try{
+            //INSTANCIA DEL OBJETO BOOK
+            Book book = new Book();
+            //CAMPOS PROPIOS DE LA TABLA BOOKS
+            book.setTitle(request.get("title").toString());
+            book.setEditorial(request.get("editorial").toString());
+            book.setDescription(request.get("description").toString());
+            book.setYearPublication(Integer.parseInt(request.get("yearPublication").toString()));
+            book.setPriceTime(new BigDecimal(request.get("priceTime").toString()));
+
+            //CAMPOS DE LA LLAVES FORANEAS
+            Author author = authorImp.findById(Long.parseLong(request.get("fkIdAuthor").toString()));
+            book.setAuthor(author);
+
+            Genre genre = genreImp.findById(Long.parseLong(request.get("fkIdGenre").toString()));
+            book.setGenre(genre);
+
+            this.bookImp.create(book);
+
+            response.put("status","success");
+            response.put("data","Registro Exitoso");
+        }catch (Exception e){
+            response.put("status",HttpStatus.BAD_GATEWAY);
+            response.put("data", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_GATEWAY);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
