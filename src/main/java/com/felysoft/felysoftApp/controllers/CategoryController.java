846f08b78537bafa024a2b6e6af17adaa1ea8b0d
MiguelC29 <miguelito28c@gmail.com>
2024-03-09 17:47:07 -0500

    add controllers of Category, Inventory, Novelty, Provider, Product, Rol with method findAll and Create

---
+++ src/main/java/com/felysoft/felysoftApp/controllers/CategoryController.java
@@ -0,0 +1,59 @@
+package com.felysoft.felysoftApp.controllers;
+
+import com.felysoft.felysoftApp.entities.Category;
+import com.felysoft.felysoftApp.services.imp.CategoryImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController //api rest
+@RequestMapping(path = "/api/category/", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.HEAD})
+@CrossOrigin("*")
+public class CategoryController {
+
+    @Autowired
+    private CategoryImp categoryImp;
+    @GetMapping("all")
+    public ResponseEntity<Map<String, Object>> findAll() {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            List<Category> categoryList = this.categoryImp.findAll();
+            response.put("status", "success");
+            response.put("data", categoryList);
+        } catch (Exception e) {
+            response.put("status", HttpStatus.BAD_GATEWAY);
+            response.put("data", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_GATEWAY);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<Map<String, Object>> create(@RequestBody Map<String, Object> request) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            // System.out.println("@@@@" + request);
+            // INSTANCIA OBJETO CATEGORIA
+            Category category = new Category();
+
+            // CAMPOS PROPIOS ENTIDAD CATEGORIA
+            //category.setIdCategory(Long.parseLong(get("id").toString()));
+            category.setName(request.get("name").toString());
+
+            this.categoryImp.create(category);
+
+            response.put("status", "success");
+            response.put("data", "Registro Exitoso");
+        } catch (Exception e) {
+            response.put("status", HttpStatus.BAD_GATEWAY);
+            response.put("data", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_GATEWAY);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/felysoft/felysoftApp/controllers/InventoryController.java
@@ -0,0 +1,95 @@
+package com.felysoft.felysoftApp.controllers;
+
+import com.felysoft.felysoftApp.entities.Book;
+import com.felysoft.felysoftApp.entities.Inventory;
+import com.felysoft.felysoftApp.entities.NoveltyInv;
+import com.felysoft.felysoftApp.entities.Product;
+import com.felysoft.felysoftApp.services.imp.BookImp;
+import com.felysoft.felysoftApp.services.imp.InventoryImp;
+import com.felysoft.felysoftApp.services.imp.NoveltyInvImp;
+import com.felysoft.felysoftApp.services.imp.ProductImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "/api/inventory/", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.HEAD})
+@CrossOrigin("*")
+public class InventoryController {
+
+    @Autowired
+    private InventoryImp inventoryImp;
+    @Autowired
+    private ProductImp productImp;
+    @Autowired
+    private BookImp bookImp;
+    @Autowired
+    private NoveltyInvImp noveltyInvImp;
+
+    @GetMapping("all")
+    public ResponseEntity<Map<String, Object>> findAll() {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            List<Inventory> inventoryList = this.inventoryImp.findAll();
+            response.put("status", "success");
+            response.put("data", inventoryList);
+        } catch (Exception e) {
+            response.put("status", HttpStatus.BAD_GATEWAY);
+            response.put("data", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_GATEWAY);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<Map<String, Object>> create(@RequestBody Map<String, Object> request) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            // INSTANCIA OBJETO INVENTARIO
+            Inventory inventory = new Inventory();
+
+            // CAMPOS PROPIOS ENTIDAD INVENTARIO
+            inventory.setStock(Integer.parseInt(request.get("stock").toString()));
+            inventory.setTypeInv(request.get("typeInv").toString());
+            inventory.setState(request.get("state").toString());
+            // Configurar fechas de creación y actualización
+            inventory.setDateRegister(new Timestamp(System.currentTimeMillis()));
+            inventory.setLastModification(new Timestamp(System.currentTimeMillis()));
+
+            // CAMPOS LLAVES FORANEAS
+            // Verificar si la clave "fkIdProduct" está presente en el mapa y no es null
+            if (request.containsKey("fkIdProduct") && request.get("fkIdProduct") != null) {
+                Product product = productImp.findById(Long.parseLong(request.get("fkIdProduct").toString()));
+                inventory.setProduct(product);
+            }
+
+            // Verificar si la clave "fkIdBook" está presente en el mapa y no es null
+            if (request.containsKey("fkIdBook") && request.get("fkIdBook") != null) {
+                Book book = bookImp.findById(Long.parseLong(request.get("fkIdBook").toString()));
+                inventory.setBook(book);
+            }
+
+            // Verificar si la clave "fkIdNovelty" está presente en el mapa y no es null
+            if (request.containsKey("fkIdNovelty") && request.get("fkIdNovelty") != null) {
+                NoveltyInv noveltyInv = noveltyInvImp.findById(Long.parseLong(request.get("fkIdNovelty").toString()));
+                inventory.setNoveltyInv(noveltyInv);
+            }
+
+            this.inventoryImp.create(inventory);
+
+            response.put("status", "success");
+            response.put("data", "Registro Exitoso");
+        } catch (Exception e) {
+            response.put("status", HttpStatus.BAD_GATEWAY);
+            response.put("data", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_GATEWAY);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
