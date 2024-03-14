35bc99fddb739de005a55322532225275c4dcc80
MiguelC29 <miguelito28c@gmail.com>
2024-03-14 17:36:53 -0500

    add CategoryRepository methods with control invertion

--- src/main/java/com/felysoft/felysoftApp/repositories/CategoryRepository.java
+++ src/main/java/com/felysoft/felysoftApp/repositories/CategoryRepository.java
@@ -4,6 +4,16 @@ import com.felysoft.felysoftApp.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    // CONSULTA CON JPQL
+    /*@Query("SELECT c FROM Category c WHERE c.eliminated = false")
+    List<Category> findAll();*/
+
+    // CONSULTA CON INVERSIÓN DE CONTROL
+    List<Category> findCategoriesByEliminatedFalse();
+    Category findCategoryByIdCategoryAndEliminatedFalse(Long id);
+
 }
