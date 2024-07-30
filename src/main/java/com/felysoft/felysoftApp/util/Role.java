64945d5f035cc7a88fd20d9bea735189db05eb93
MiguelC29 <miguelito28c@gmail.com>
2024-07-29 19:38:08 -0500

    add more roles and permissions. Insert users

--- src/main/java/com/felysoft/felysoftApp/util/Role.java
+++ src/main/java/com/felysoft/felysoftApp/util/Role.java
@@ -123,12 +123,120 @@ public enum Role {
 
             Permission.READ_MY_PROFILE
     )),
-    CUSTOMER(Arrays.asList(
+    INVENTORY_MANAGER(Arrays.asList(
             Permission.READ_ALL_PRODUCTS,
+            Permission.READ_ONE_PRODUCT,
+            Permission.CREATE_ONE_PRODUCT,
+            Permission.UPDATE_ONE_PRODUCT,
+            Permission.READ_INVENTORY_PRODUCTS,
+
+            Permission.READ_ALL_CATEGORIES,
+            Permission.READ_ONE_CATEGORY,
+            Permission.CREATE_ONE_CATEGORY,
+            Permission.UPDATE_ONE_CATEGORY,
+            Permission.READ_CATEGORIES_BY_PROVIDER,
+            Permission.ASSOCIATE_CATEGORY_PROVIDER,
+
+            Permission.READ_ALL_PROVIDERS,
+            Permission.READ_ONE_PROVIDER,
+            Permission.CREATE_ONE_PROVIDER,
+            Permission.UPDATE_ONE_PROVIDER,
+            Permission.READ_PROVIDERS_BY_CATEGORY,
+
+            Permission.READ_ALL_SERVICES,
+            Permission.READ_ONE_SERVICE,
+            Permission.CREATE_ONE_SERVICE,
+            Permission.UPDATE_ONE_SERVICE,
+
+            Permission.READ_ALL_TYPE_SERVICES,
+            Permission.READ_ONE_TYPE_SERVICE,
+            Permission.CREATE_ONE_TYPE_SERVICE,
+            Permission.UPDATE_ONE_TYPE_SERVICE,
+
+            Permission.READ_ALL_GENRES,
+            Permission.READ_ONE_GENRE,
+            Permission.CREATE_ONE_GENRE,
+            Permission.UPDATE_ONE_GENRE,
+            Permission.READ_GENRES_BY_AUTHOR,
+            Permission.ASSOCIATE_GENRE_AUTHOR,
+
+            Permission.READ_ALL_AUTHORS,
+            Permission.READ_ONE_AUTHOR,
+            Permission.CREATE_ONE_AUTHOR,
+            Permission.UPDATE_ONE_AUTHOR,
+            Permission.READ_AUTHORS_BY_GENRE,
+
+            Permission.READ_ALL_BOOKS,
+            Permission.READ_ONE_BOOK,
+            Permission.CREATE_ONE_BOOK,
+            Permission.UPDATE_ONE_BOOK,
+            Permission.READ_INVENTORY_BOOKS,
+
+            Permission.READ_ALL_DETAILS,
+            Permission.READ_ONE_DETAIL,
+            Permission.CREATE_ONE_DETAIL,
+            Permission.UPDATE_ONE_DETAIL,
+
+            Permission.READ_ALL_PURCHASES,
+            Permission.READ_ONE_PURCHASE,
+            Permission.CREATE_ONE_PURCHASE,
+            Permission.UPDATE_ONE_PURCHASE,
+            Permission.READ_EXPENSE_BY_PURCHASE,
+
             Permission.READ_MY_PROFILE
     )),
-    USER(Arrays.asList(
-            Permission.READ_ALL_PRODUCTS,
+    FINANCIAL_MANAGER(Arrays.asList(
+            Permission.READ_ALL_PAYMENTS,
+
+            Permission.READ_ALL_DETAILS,
+
+            Permission.READ_ALL_EXPENSES,
+            Permission.READ_ONE_EXPENSE,
+            Permission.CREATE_ONE_EXPENSE,
+            Permission.UPDATE_ONE_EXPENSE,
+
+            Permission.READ_ALL_SALES,
+
+            Permission.READ_ALL_PURCHASES,
+
+            Permission.READ_MY_PROFILE
+    )),
+    SALESPERSON(Arrays.asList(
+            Permission.READ_INVENTORY_PRODUCTS,
+
+            Permission.READ_ALL_SERVICES,
+            Permission.READ_ALL_TYPE_SERVICES,
+
+            Permission.READ_ALL_GENRES,
+            Permission.READ_ALL_AUTHORS,
+            Permission.READ_ALL_BOOKS,
+
+            Permission.READ_ALL_PAYMENTS,
+            Permission.CREATE_ONE_PAYMENT,
+
+            Permission.READ_ALL_DETAILS,
+            Permission.READ_ONE_DETAIL,
+            Permission.CREATE_ONE_DETAIL,
+
+            Permission.READ_ALL_SALES,
+            Permission.READ_ONE_SALE,
+            Permission.CREATE_ONE_SALE,
+
+            Permission.READ_ALL_RESERVES,
+            Permission.READ_ONE_RESERVE,
+            Permission.CREATE_ONE_RESERVE,
+            Permission.UPDATE_ONE_RESERVE,
+
+            Permission.READ_MY_PROFILE
+    )),
+    CUSTOMER(Arrays.asList(
+            Permission.READ_ONE_RESERVE,
+            Permission.CREATE_ONE_RESERVE,
+
+            Permission.READ_ALL_GENRES,
+            Permission.READ_ALL_AUTHORS,
+            Permission.READ_ALL_BOOKS,
+
             Permission.READ_MY_PROFILE
     ));
 
