86e326537f19939b8ced0fc5d5fd066e3342940f
MiguelC29 <miguelito28c@gmail.com>
2024-09-06 21:12:00 -0500

    update cords configuration

--- src/main/java/com/felysoft/felysoftApp/config/CorsConfiguration.java
+++ src/main/java/com/felysoft/felysoftApp/config/CorsConfiguration.java
@@ -9,7 +9,10 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://felysoft-react.vercel.app/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                .allowedOrigins("https://felysoft-react.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowedHeaders("Authorization", "Content-Type") // Permitir el envío del JWT y otros headers
+                .exposedHeaders("Authorization") // Exponer el header si necesitas que el frontend lo lea en la respuesta
+                .maxAge(3600); // Tiempo de caché de las políticas de CORS
     }
 }
